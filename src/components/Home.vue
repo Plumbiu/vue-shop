91b5dfa18c683e7d0e112b5ceb28bdb30926f009
Plumbiu <3434909403@qq.com>
2022-07-09 19:58:31 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <el-button tyle="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
